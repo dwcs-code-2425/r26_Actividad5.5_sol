f465a65fb7fa6e7d54bad48433f0184a24d53440
cd-dist-2324 <cd.dist.2324@gmail.com>
2026-03-27 17:53:00 +0100

    1-4 apartados 5.2

--- src/Controller/ApiLibroController.php
+++ src/Controller/ApiLibroController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Libro;
 use App\Repository\LibroRepository;
 
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 
@@ -24,4 +27,53 @@ final class ApiLibroController extends AbstractController
         return $this->json($repo->findAll());
     }
 
+    //Obtener un libro por su id
+    #[Route('/libros/{id}', name: 'libros_id', methods: ['GET'])]
+    public function show(LibroRepository $repo, int $id): JsonResponse
+    {
+        $libro = $repo->find($id);
+        if (!$libro) {
+            return $this->json(['error' => 'Libro not found'], 404);
+        }
+        return $this->json($libro);
+    }
+
+
+    //Eliminar un libro por su id
+    #[Route('/libros/{id}', name: 'libros_delete', methods: ['DELETE'])]
+    public function delete(LibroRepository $repo, int $id, EntityManagerInterface $em): JsonResponse
+    {
+        $libro = $repo->find($id);
+        if (!$libro) {
+            return $this->json(['error' => 'Libro not found'], 404);
+        }
+        $em->remove($libro);
+        $em->flush();
+
+
+        return $this->json(null, 204);
+    }
+
+    //Crear libro
+    #[Route('/libros', name: 'libros_create', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        if ($data === null) {
+            return $this->json(['error' => 'Invalid JSON'], 400);
+        }
+        if (isset($data["titulo"]) && !empty(trim($data["titulo"]))) {
+            $libro = new Libro();
+            $libro->setTitulo($data["titulo"]);
+
+            $em->persist($libro);
+            $em->flush();
+
+            return $this->json($libro, 201);
+        } else {
+            return $this->json(['error' => 'El campo titulo es obligatorio'], 400);
+        }
+
+
+    }
 }
